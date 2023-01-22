33e52d152a2726f584b753a10c488a8d822d4cca
sreynoso <s.reynosodunjo@gmail.com>
2023-01-21 22:29:38 -0300

    Cambios note

---
+++ src/services/getAPIConfig.ts
@@ -0,0 +1,7 @@
+type T_APIConfig = {
+    readonly API_KEY: string
+}
+
+export const APIConfig: T_APIConfig = {
+    API_KEY: "YolxJfVQQOI0uHqTgTPKfYd19RypDVRM",
+}
--- src/services/getGifs.ts
+++ src/services/getGifs.ts
@@ -1,13 +1,20 @@
-const API_KEY = "YolxJfVQQOI0uHqTgTPKfYd19RypDVRM";
+import { APIConfig } from "./getAPIConfig";
 
-const getGifs = function (keyword, limit) {
+type Gif = {
+    title: string,
+    id: string,
+    url: string,
+}
+
+export const getGifs = function (keyword: string, limit: number) {
+    const API_KEY = APIConfig.API_KEY
     const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`;
 
     return fetch(API_URL)
         .then((response) => response.json())
         .then((response) => {
             const { data } = response;
-            const gifs = data.map((image) => {
+            const gifs: Gif[] = data.map((image: any) => {
                 const { images, title, id } = image;
                 const { url } = images.downsized_medium;
                 return { title, id, url };
@@ -15,5 +22,3 @@ const getGifs = function (keyword, limit) {
             return gifs;
         });
 };
-
-export default getGifs;
